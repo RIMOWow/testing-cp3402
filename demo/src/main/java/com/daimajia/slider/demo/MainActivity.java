238b69bf5eb35bbbf92dcb15df98587b239d0dbf
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-01-19 10:42:27 +0000

    Added badges to README.md

---
+++ demo/src/main/java/com/daimajia/slider/demo/MainActivity.java
@@ -0,0 +1,10 @@
+
+package com.daimajia.slider.demo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
