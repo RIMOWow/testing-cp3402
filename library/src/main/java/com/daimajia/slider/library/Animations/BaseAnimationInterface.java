c1f804f1b6b98ed1f36a63ecbbd9082dc239d3a9
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-03-20 11:43:44 +0000

    add license information

--- library/src/main/java/com/daimajia/slider/library/Animations/BaseAnimationInterface.java
+++ library/src/main/java/com/daimajia/slider/library/Animations/BaseAnimationInterface.java
@@ -3,4 +3,5 @@ package com.daimajia.slider.library.Animations;
 import android.view.View;
 
 /**
- * This interface gives you chance to inject your own anim
+ * This interface gives you chance to inject your own animation or do something when the
+ * {@link com.daimajia.slider.library.Tric
