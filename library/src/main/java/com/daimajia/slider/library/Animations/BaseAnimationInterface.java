55aa2ef927f38335a9ff5d690ec26254fc4e14c4
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-03-20 21:32:26 +0000

    add new readme

--- library/src/main/java/com/daimajia/slider/library/Animations/BaseAnimationInterface.java
+++ library/src/main/java/com/daimajia/slider/library/Animations/BaseAnimationInterface.java
@@ -4,4 +4,7 @@ import android.view.View;
 
 /**
  * This interface gives you chance to inject your own animation or do something when the
- * {@link com.daimajia.slider.library.Tric
+ * {@link com.daimajia.slider.library.Tricks.ViewPagerEx} animation (PagerTransformer) starts or ends.
+ *
+ *
+ * There are t
