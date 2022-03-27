ed4fba1b9632eda48b7fd8d871b08a6eb3b11eda
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-03-27 13:04:21 +0000

    drm/i915: Add missing MacBook Pro models with dual channel LVDS

--- library/src/main/java/com/daimajia/slider/library/Animations/DescriptionAnimation.java
+++ library/src/main/java/com/daimajia/slider/library/Animations/DescriptionAnimation.java
@@ -4,4 +4,8 @@ import android.view.View;
 
 import com.daimajia.slider.library.R;
 import com.nineoldandroids.animation.ObjectAnimator;
-import com.nineoldandroids.animation.ValueAnimat
+import com.nineoldandroids.animation.ValueAnimator;
+import com.nineoldandroids.view.ViewHelper;
+
+/**
+ * A demo
