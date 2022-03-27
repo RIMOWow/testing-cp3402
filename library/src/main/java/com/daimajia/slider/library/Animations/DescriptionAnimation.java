96b4734d71453756af39239d487ddaeeab77806f
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-03-27 11:41:33 +0000

    Fixes and refactoring

--- library/src/main/java/com/daimajia/slider/library/Animations/DescriptionAnimation.java
+++ library/src/main/java/com/daimajia/slider/library/Animations/DescriptionAnimation.java
@@ -3,4 +3,5 @@ package com.daimajia.slider.library.Animations;
 import android.view.View;
 
 import com.daimajia.slider.library.R;
-import com.nineoldandroids.animati
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.animation.ValueAnimat
