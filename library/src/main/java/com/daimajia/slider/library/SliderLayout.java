9a3a8b09f050d9ad245ff0160c667095381a9ad6
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-05-23 15:12:05 +0000

    Stop using intersphinx

--- library/src/main/java/com/daimajia/slider/library/SliderLayout.java
+++ library/src/main/java/com/daimajia/slider/library/SliderLayout.java
@@ -63,3 +63,86 @@ import java.util.TimerTask;
  *
  * indicator_unselected_drawable
  *
+ * pager_animation
+ *      Default
+ *      Accordion
+ *      Background2Foreground
+ *      CubeIn
+ *      DepthPage
+ *      Fade
+ *      FlipHorizontal
+ *      FlipPage
+ *      Foreground2Background
+ *      RotateDown
+ *      RotateUp
+ *      Stack
+ *      Tablet
+ *      ZoomIn
+ *      ZoomOutSlide
+ *      ZoomOut
+ *
+ * pager_animation_span
+ *
+ *
+ */
+public class SliderLayout extends RelativeLayout{
+
+    private Context mContext;
+    /**
+     * InfiniteViewPager is extended from ViewPagerEx. As the name says, it can scroll without bounder.
+     */
+    private InfiniteViewPager mViewPager;
+
+    /**
+     * InfiniteViewPager adapter.
+     */
+    private SliderAdapter mSliderAdapter;
+
+    /**
+     * {@link com.daimajia.slider.library.Tricks.ViewPagerEx} indicator.
+     */
+    private PagerIndicator mIndicator;
+
+
+    /**
+     * A timer and a TimerTask using to cycle the {@link com.daimajia.slider.library.Tricks.ViewPagerEx}.
+     */
+    private Timer mCycleTimer;
+    private TimerTask mCycleTask;
+
+    /**
+     * For resuming the cycle, after user touch or click the {@link com.daimajia.slider.library.Tricks.ViewPagerEx}.
+     */
+    private Timer mResumingTimer;
+    private TimerTask mResumingTask;
+
+    /**
+     * If {@link com.daimajia.slider.library.Tricks.ViewPagerEx} is Cycling
+     */
+    private boolean mCycling;
+
+    /**
+     * Determine if auto recover after user touch the {@link com.daimajia.slider.library.Tricks.ViewPagerEx}
+     */
+    private boolean mAutoRecover = true;
+
+    private int mTransformerId;
+
+    /**
+     * {@link com.daimajia.slider.library.Tricks.ViewPagerEx} transformer time span.
+     */
+    private int mTransformerSpan = 1100;
+
+    private boolean mAutoCycle;
+
+    /**
+     * the duration between animation.
+     */
+    private long mSliderDuration = 4000;
+
+    /**
+     * Visibility of {@link com.daimajia.slider.library.Indicators.PagerIndicator}
+     */
+    private PagerIndicator.IndicatorVisibility mIndicatorVisibility = PagerIndicator.IndicatorVisibility.Visible;
+
+    /**
