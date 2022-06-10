0b7be899b8d141577f8e01187727c1a1063f9288
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-06-10 08:37:50 +0000

    2004-01-07  Michael Chastain  <mec.gnu@mindspring.com>

--- library/src/main/java/com/daimajia/slider/library/SliderTypes/BaseSliderView.java
+++ library/src/main/java/com/daimajia/slider/library/SliderTypes/BaseSliderView.java
@@ -16,3 +16,87 @@ import java.io.File;
 /**
  * When you want to make your own slider view, you must extends from this class.
  * BaseSliderView provides some useful methods.
+ * I provide two example: {@link com.daimajia.slider.library.SliderTypes.DefaultSliderView} and
+ * {@link com.daimajia.slider.library.SliderTypes.TextSliderView}
+ * if you want to show progressbar, you just need to set a progressbar id as @+id/loading_bar.
+ */
+public abstract class BaseSliderView {
+
+    protected Context mContext;
+
+    private Bundle mBundle;
+
+    /**
+     * Error place holder image.
+     */
+    private int mErrorPlaceHolderRes;
+
+    /**
+     * Empty imageView placeholder.
+     */
+    private int mEmptyPlaceHolderRes;
+
+    private String mUrl;
+    private File mFile;
+    private int mRes;
+
+    protected OnSliderClickListener mOnSliderClickListener;
+
+    private boolean mErrorDisappear;
+
+    private ImageLoadListener mLoadListener;
+
+    private String mDescription;
+
+    private Picasso mPicasso;
+
+    /**
+     * Scale type of the image.
+     */
+    private ScaleType mScaleType = ScaleType.Fit;
+
+    public enum ScaleType{
+        CenterCrop, CenterInside, Fit, FitCenterCrop
+    }
+
+    protected BaseSliderView(Context context) {
+        mContext = context;
+    }
+
+    /**
+     * the placeholder image when loading image from url or file.
+     * @param resId Image resource id
+     * @return
+     */
+    public BaseSliderView empty(int resId){
+        mEmptyPlaceHolderRes = resId;
+        return this;
+    }
+
+    /**
+     * determine whether remove the image which failed to download or load from file
+     * @param disappear
+     * @return
+     */
+    public BaseSliderView errorDisappear(boolean disappear){
+        mErrorDisappear = disappear;
+        return this;
+    }
+
+    /**
+     * if you set errorDisappear false, this will set a error placeholder image.
+     * @param resId image resource id
+     * @return
+     */
+    public BaseSliderView error(int resId){
+        mErrorPlaceHolderRes = resId;
+        return this;
+    }
+
+    /**
+     * the description of a slider image.
+     * @param description
+     * @return
+     */
+    public BaseSliderView description(String description){
+        mDescription = description;
