f91267f724ae7792a40e2c13db80dea0abf8a9e0
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-07-29 13:41:10 +0000

    Removing unnecessary code.

---
+++ library/src/main/java/com/daimajia/slider/library/Transformers/BaseTransformer.java
@@ -0,0 +1,13 @@
+
+package com.daimajia.slider.library.Transformers;
+
+import android.view.View;
+
+import com.daimajia.slider.library.Animations.BaseAnimationInterface;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.nineoldandroids.view.ViewHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
