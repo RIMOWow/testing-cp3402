0fe8f14d51eeb3c53d55153749f7b5dd3f177f25
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-08-06 22:15:13 +0000

    msm: null pointer dereferencing

--- library/src/main/java/com/daimajia/slider/library/Transformers/FadeTransformer.java
+++ library/src/main/java/com/daimajia/slider/library/Transformers/FadeTransformer.java
@@ -19,4 +19,6 @@ public class FadeTransformer extends BaseTransformer {
         // Page is sibling to left or right
         else if (position <= 0 || position <= 1) {
 
-            // Calculate alpha.  Position is decimal in [-1
+            // Calculate alpha.  Position is decimal in [-1,0] or [0,1]
+            float alpha = (position <= 0) ? position + 1 : 1 - position;
+   
