d2106ace5cc186e6b5e88433a1c4779b58a3963a
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-08-09 05:31:29 +0000

    new pages

--- library/src/main/java/com/daimajia/slider/library/Transformers/FlipPageViewTransformer.java
+++ library/src/main/java/com/daimajia/slider/library/Transformers/FlipPageViewTransformer.java
@@ -3,4 +3,7 @@ package com.daimajia.slider.library.Transformers;
 import android.os.Build;
 import android.view.View;
 
-import com.daimajia.slider.libra
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.nineoldandroids.view.ViewHelper;
+
+public 
