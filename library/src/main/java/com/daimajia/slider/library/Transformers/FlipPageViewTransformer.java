a7e845479c7772b03773228db89e39f976576e65
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-08-17 20:38:48 +0000

    continue testing

--- library/src/main/java/com/daimajia/slider/library/Transformers/FlipPageViewTransformer.java
+++ library/src/main/java/com/daimajia/slider/library/Transformers/FlipPageViewTransformer.java
@@ -36,4 +36,7 @@ public class FlipPageViewTransformer extends BaseTransformer {
 
     private void setSize(View view, float position, float percentage) {
         ViewHelper.setScaleX(view,(position != 0 && position != 1) ? percentage : 1);
-        ViewHel
+        ViewHelper.setScaleY(view,(position != 0 && position != 1) ? percentage : 1);
+    }
+
+    private void 
