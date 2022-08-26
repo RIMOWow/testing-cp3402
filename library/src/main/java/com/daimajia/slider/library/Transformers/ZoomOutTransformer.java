7380fdc55dcac260339f141b1d3968ac0526b5ee
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-08-26 19:55:53 +0000

    Add citation

--- library/src/main/java/com/daimajia/slider/library/Transformers/ZoomOutTransformer.java
+++ library/src/main/java/com/daimajia/slider/library/Transformers/ZoomOutTransformer.java
@@ -9,4 +9,5 @@ public class ZoomOutTransformer extends BaseTransformer {
     @Override
     protected void onTransform(View view, float position) {
         final float scale = 1f + Math.abs(position);
-    
+        ViewHelper.setScaleX(view,scale);
+        ViewHelper.setScaleY(view,scale);
