c03d3d7a2605eb0ef8bd833775052647c859822e
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-08-30 10:35:39 +0000

    Add latest Ansible version (2.6.0)

--- library/src/main/java/com/daimajia/slider/library/Tricks/InfiniteViewPager.java
+++ library/src/main/java/com/daimajia/slider/library/Tricks/InfiniteViewPager.java
@@ -1,4 +1,8 @@
 package com.daimajia.slider.library.Tricks;
 
 import android.content.Context;
-import android.support.v4.view.PagerAd
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/
