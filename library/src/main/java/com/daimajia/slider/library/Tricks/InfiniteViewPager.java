542c359b6fbbf00685473e7ad2cd9baf69c8ef45
RIMOWow <97898561+RIMOWow@users.noreply.github.com>
2022-08-30 12:52:51 +0000

    Merging develop

--- library/src/main/java/com/daimajia/slider/library/Tricks/InfiniteViewPager.java
+++ library/src/main/java/com/daimajia/slider/library/Tricks/InfiniteViewPager.java
@@ -5,4 +5,5 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 
-/
+/**
+ * A {@link ViewPager} that allows pseudo-infinite paging with a w
